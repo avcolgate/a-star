f930dfd57f82aea2a6e22f869d12378946346aeb
avcolgate <avcolgate@gmail.com>
2022-12-26 23:22:43 +0300

    window size fix & size errors fix

--- main.py
+++ main.py
@@ -39,17 +39,18 @@ for num, cord in enumerate(wall_coords):
 # print(path_coords)
 # print(wall_coords)
 
-fig = plt.figure(figsize =([matrix.width*0.51, matrix.height*0.51]) )
+node_size = 1000
+fig = plt.figure(figsize =([matrix.width*node_size/2150, matrix.height*node_size/2150]) )
 
-nx.draw_networkx_nodes(my_graph, all_coords, nodelist=grid_dict, node_color="tab:gray", node_shape='s', node_size=1200, alpha=0.1)
-nx.draw_networkx_nodes(my_graph, wall_coords, nodelist=wall_dict, node_color="black", node_shape='s', node_size=1200, alpha=0.9)
-nx.draw_networkx_nodes(my_graph, path_coords, nodelist=path_dict, node_color="orange", node_shape='s', node_size=1200, alpha=0.7)
+nx.draw_networkx_nodes(my_graph, all_coords, nodelist=grid_dict, node_color="tab:gray", node_shape='s', node_size=node_size, alpha=0.1)
+nx.draw_networkx_nodes(my_graph, wall_coords, nodelist=wall_dict, node_color="black", node_shape='s', node_size=node_size, alpha=0.9)
+nx.draw_networkx_nodes(my_graph, path_coords, nodelist=path_dict, node_color="orange", node_shape='s', node_size=node_size, alpha=0.7)
 
 labels = {0: 'start', 1: 'goal'}
-nx.draw_networkx_nodes(my_graph, main_coords, nodelist=[0], node_color="tab:green", node_shape='o', node_size=1200, alpha=0.9)
-nx.draw_networkx_nodes(my_graph, main_coords, nodelist=[1], node_color="tab:red", node_shape='o', node_size=1200, alpha=0.9)
+nx.draw_networkx_nodes(my_graph, main_coords, nodelist=[0], node_color="tab:green", node_shape='o', node_size=node_size*2, alpha=0.9)
+nx.draw_networkx_nodes(my_graph, main_coords, nodelist=[1], node_color="tab:red", node_shape='o', node_size=node_size*2, alpha=0.9)
 
-nx.draw_networkx_labels(my_graph, main_coords, labels, font_size=14, font_color="whitesmoke", font_family="sans-serif")
+nx.draw_networkx_labels(my_graph, main_coords, labels, font_size=16, font_color="whitesmoke", font_family="sans-serif")
 
 plt.tight_layout()
 plt.axis("off")
